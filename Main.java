d90cacec9b3f042db7191ffb728eb177bda09d86
Kyle Vu <ktvu@hbuhsd.edu>
2024-10-27 19:34:43 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,23 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    // TODO: Problem 1
+
+
+    // TODO: Problem 2
+
+
+    // TODO: Problem 3
+  }
+
+  // TODO: In-class assignment
+  // Create a method that takes in two Rectangles and returns
+  // true if they are equal, and false otherwise.  Two
+  // Rectangles are considered equal if their widths are equal,
+  // and if their lengths are equal.
+  public static boolean rectEqual(Rectangle r1, Rectangle r2)
+  {
+    // implement solution here
+    return false;
   }
 }
